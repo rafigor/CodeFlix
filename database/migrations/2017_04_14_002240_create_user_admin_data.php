4b45ab1fb3797d4614f36a0d2bc6646063c743d2
Rafael Oliveira <rafigo@gmail.com>
2017-04-13 21:36:22 -0300

    Usuário Admin

---
+++ database/migrations/2017_04_14_002240_create_user_admin_data.php
@@ -0,0 +1,36 @@
+<?php
+
+use CodeFlix\Models\User;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserAdminData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        User::created([
+            'name' => env('ADMIN_DEFAULT_NAME','Administrator'),
+            'email' => env('ADMIN_DEFAULT_EMAIL','admin@user.com'),
+            'role' => User::ROLE_ADMIN,
+            'password' => bcrypt(env('ADMIN_DEFAULT_PASSWORD','secret'))
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $user = User::where('email','=',env('ADMIN_DEFAULT_EMAIL','admin@user.com'))->first();
+        if ($user instanceof User){
+            $user->delete();
+        }
+
+    }
+}
